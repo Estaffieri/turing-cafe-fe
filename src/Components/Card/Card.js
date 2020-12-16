b378d3b86843a826237288669e799ea08e8219d1
Estelle Staffieri <estellelareinne@gmail.com>
2020-12-16 09:57:49 -0700

    Get reservations from API displaying on the main page

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Card.css"
+
+const Card = ({reservation}) => {
+    return(
+        <section key={reservation.id} className="card">
+            <h3>Name:{reservation.name}</h3>
+            <p>Date:{reservation.date}</p>
+            <p>Time:{reservation.time}</p>
+            <p>Number of guests:{reservation.number}</p>
+        </section>
+    )
+}
+
+export default Card;
+
