e7a8d78383723887fd118b18e6ac540d56ae0030
Estelle Staffieri <estellelareinne@gmail.com>
2020-12-16 10:33:11 -0700

    Add form component and get form rendered on app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Card from "../Card/Card";
+import Form from "../Form/Form";
 import { getAllReservations } from "../../apiCalls"
 
 class App extends Component {
@@ -30,6 +31,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
+          <Form />
 
         </div>
         <div className='resy-container'>
